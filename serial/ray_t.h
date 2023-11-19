7e53ab69072c1e4882d6190896bd72c2256896cf
Jacky Huang <lashhw81@gmail.com>
2023-11-19 15:21:58 +0800

    .

--- serial/ray_t.h
+++ serial/ray_t.h
@@ -3,11 +3,7 @@
 
 #include "vec3_t.h"
 
-class ray_t {
-public:
-    ray_t(const vec3_t &origin, const vec3_t &direction, float t_min, float t_max) :
-        origin(origin), direction(direction), t_min(t_min), t_max(t_max) {}
-
+struct ray_t {
     vec3_t at(float t) const {
         return origin + t * direction;
     }
